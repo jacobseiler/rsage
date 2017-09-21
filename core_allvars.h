2f1d5629e0cfe2a60d1299985c9d6f4ef8fbae85
jacobseiler <jseiler@swin.edu.au>
2017-09-21 17:39:50 +1000

    Changed 'update_grid_array' to use the Galaxy's snapshot rather than the halo snapshot.

--- core_allvars.h
+++ core_allvars.h
@@ -12,7 +12,7 @@ do {                                                                \
   myexit(sigterm);                                                \
 } while(0)
 
-#define  STEPS 1        // Number of integration intervals between two snapshots
+#define  STEPS 10        // Number of integration intervals between two snapshots
 #define  MAXGALFAC 1
 #define  ALLOCPARAMETER 10.0
 #define  MAX_NODE_NAME_LEN 50
