1f932c701ceb27b3b45dd9b4bfc85962c6aace24
jacobseiler <jseiler@swin.edu.au>
2018-06-09 12:05:29 +1000

    Redid the reduction of the gas through the ejection channels.
    Previously, I subtracted an equal amounts from both the SN and QSO channels.  However
    this doesn't really make physical sense.  Consider if the ejected reservoir was composed
    of 90% SN gas and 10% QSO gas.  During a reincorporation/infall event it then doesn't make
    sense to give an equal weight to both components.
    
    Instead what I've doen is now decrease the channels in proportion to the ratio of SN/QSO gas
    in the ejected reservoirs.  This commit readded the XASSERT statements and the tests pass.

--- sage/model_reincorporation.c
+++ sage/model_reincorporation.c
@@ -17,6 +17,8 @@ void reincorporate_gas(int centralgal, double dt)
   // SN velocity is 630km/s, and the condition for reincorporation is that the 
   // halo has an escape velocity greater than this, i.e. V_SN/sqrt(2) = 445.48km/s
   double Vcrit = 445.48 * ReIncorporationFactor;  
+  double channel_fractionSN, channel_fractionQSO;
+  channel_fractionSN = channel_fractionQSO = 0.0;
 	
   if(Gal[centralgal].Vvir > Vcrit)
   {
@@ -29,8 +31,19 @@ void reincorporate_gas(int centralgal, double dt)
 
     metallicity = get_metallicity(Gal[centralgal].EjectedMass, Gal[centralgal].MetalsEjectedMass);
     dust_fraction_ejected = get_dust_fraction(Gal[centralgal].EjectedMass, Gal[centralgal].DustEjectedMass);
-    
-  
+   
+    if (Gal[centralgal].EjectedMassSN > 0.0)
+    {
+      channel_fractionSN = Gal[centralgal].EjectedMassSN / Gal[centralgal].EjectedMass; 
+    }
+
+    if (Gal[centralgal].EjectedMassQSO > 0.0)
+    { 
+      channel_fractionQSO =  Gal[centralgal].EjectedMassQSO /  Gal[centralgal].EjectedMass; 
+    }
+
+    //printf("Ejected %.4e\tSN %.4e\tQSO %.4e\tSNFrac %.4e\tQSOFrac %.4e\n", Gal[centralgal].EjectedMass, Gal[centralgal].EjectedMassSN, Gal[centralgal].EjectedMassQSO, channel_fractionSN, channel_fractionQSO);
+ 
     Gal[centralgal].EjectedMass -= reincorporated;
     Gal[centralgal].MetalsEjectedMass -= metallicity * reincorporated;
     Gal[centralgal].DustEjectedMass -= dust_fraction_ejected * reincorporated;
@@ -38,45 +51,14 @@ void reincorporate_gas(int centralgal, double dt)
     Gal[centralgal].MetalsHotGas += metallicity * reincorporated;
     Gal[centralgal].DustHotGas += dust_fraction_ejected * reincorporated;
 
-    Gal[centralgal].EjectedMassSN -= reincorporated/2.0;
-    Gal[centralgal].EjectedMassQSO -= reincorporated/2.0;
+    Gal[centralgal].EjectedMassSN = Gal[centralgal].EjectedMass * channel_fractionSN;
+    Gal[centralgal].EjectedMassQSO = Gal[centralgal].EjectedMass * channel_fractionQSO;
     
-    // In the case that one of the channels did not enough gas to be fully reincorporated, we subtract the
-    // remaining gas from the other channel.
-    if (Gal[centralgal].EjectedMassSN < 0.0 && Gal[centralgal].EjectedMassQSO < 0.0)
-    {
-      Gal[centralgal].EjectedMassSN = Gal[centralgal].EjectedMassQSO = 0.0;
-    }
-    else if (Gal[centralgal].EjectedMassSN < 0.0)
-    {
-      Gal[centralgal].EjectedMassQSO += Gal[centralgal].EjectedMassSN;
-      Gal[centralgal].EjectedMassSN = 0.0;
-
-      if (Gal[centralgal].EjectedMassQSO <= 0.0 && Gal[centralgal].EjectedMass < 1.0e-6) 
-      {
-        Gal[centralgal].EjectedMassQSO = Gal[centralgal].EjectedMass;
-      }
-
-    }
-    else if (Gal[centralgal].EjectedMassQSO < 0.0)
-    {
-      Gal[centralgal].EjectedMassSN += Gal[centralgal].EjectedMassQSO;
-      Gal[centralgal].EjectedMassQSO = 0.0;
-
-      if (Gal[centralgal].EjectedMassSN <= 0.0 && Gal[centralgal].EjectedMass < 1.0e-6) 
-      {
-        Gal[centralgal].EjectedMassSN = Gal[centralgal].EjectedMass;
-      }
-
-    }
-
     if (Gal[centralgal].DustEjectedMass < 0.0)
       Gal[centralgal].DustEjectedMass = 0.0;
 
-    //if (Gal[centralgal].EjectedMass > 1e-10)
-    //  XASSERT(Gal[centralgal].EjectedMass / (Gal[centralgal].EjectedMassSN + Gal[centralgal].EjectedMassQSO) > 0.95, "EjectedMass %.4e\tSN %.4e\tQSO %.4e\tRatio %.4e\n", Gal[centralgal].EjectedMass, Gal[centralgal].EjectedMassSN, Gal[centralgal].EjectedMassQSO, Gal[centralgal].EjectedMass / (Gal[centralgal].EjectedMassSN + Gal[centralgal].EjectedMassQSO));
-
-
+    if (Gal[centralgal].EjectedMass > 1e-10)
+      XASSERT(Gal[centralgal].EjectedMass / (Gal[centralgal].EjectedMassSN + Gal[centralgal].EjectedMassQSO) > 0.95, "EjectedMass %.4e\tSN %.4e\tQSO %.4e\tRatio %.4e\n", Gal[centralgal].EjectedMass, Gal[centralgal].EjectedMassSN, Gal[centralgal].EjectedMassQSO, Gal[centralgal].EjectedMass / (Gal[centralgal].EjectedMassSN + Gal[centralgal].EjectedMassQSO));
   }
 
 }
