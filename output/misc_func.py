e88adefa13ae4db685e9a94e46ef26e8cb7ded03
jacobseiler <jseiler@swin.edu.au>
2018-08-28 00:43:29 +1000

    Quick functions to plot fesc as a function of MH for mine and Anne's models.

--- output/misc_func.py
+++ output/misc_func.py
@@ -89,11 +89,100 @@ def determine_close_idx(fname_HII, fname_density, SnapList, GridSize,
     return SnapList 
 
 
-def determine_MH_fesc_constants(low_MH, low_fesc, high_MH, high_fesc):
+def determine_MH_fesc_constants(MH_low, MH_high, fesc_low, fesc_high):
     
-    log_A = (np.log10(high_fesc) - (np.log10(low_fesc)*np.log10(high_MH)/np.log10(low_MH))) * pow(1 - (np.log10(high_MH) / np.log10(low_MH)), -1)
-    B = (np.log10(low_fesc) - log_A) / np.log10(low_MH)
+    log_A = (np.log10(fesc_high) - (np.log10(fesc_low)*np.log10(MH_high)/np.log10(MH_low))) * pow(1 - (np.log10(MH_high) / np.log10(MH_low)), -1)
+    B = (np.log10(fesc_low) - log_A) / np.log10(MH_low)
     A = pow(10, log_A)
 
     return A, B
 
+
+def plot_Anne_MH(MH_low, MH_high, fesc_low, fesc_high, pos_scaling, ax1):
+
+    halomass = np.arange(7.0, 18.0, 0.01)
+    halomass = pow(10, halomass)
+
+    if pos_scaling:
+        fesc = 1.0 - pow((1.0 - fesc_low) * (1.0-fesc_low)/(1.0-fesc_high), -np.log10(halomass/MH_low)/np.log10(MH_high/MH_low))
+        fesc[fesc < fesc_low] = fesc_low
+        fesc[fesc > fesc_high] = fesc_high
+    else:
+        fesc = pow(fesc_low * fesc_low/fesc_high, -np.log10(halomass/MH_low)/np.log10(MH_high/MH_low))
+        fesc[fesc > fesc_low] = fesc_low
+        fesc[fesc < fesc_high] = fesc_high
+
+    ax1.plot(np.log10(halomass),
+             fesc, ls = '-', color = 'k',
+             label = "Anne")
+
+    ax1.set_xlabel("Halo Mass [Msun]")
+    ax1.set_ylabel("fesc")
+
+    ax1.set_ylim([0.0, 1.1])
+
+    return ax1
+
+
+def plot_my_MH(MH_low, MH_high, fesc_low, fesc_high, ax1):
+
+    halomass = np.arange(7.0, 18.0, 0.01)
+    halomass = pow(10, halomass)
+
+    alpha, beta = determine_MH_fesc_constants(MH_low, MH_high,
+                                              fesc_low, fesc_high)
+
+    print("Alpha = {0} Beta = {1}".format(alpha, beta))
+    fesc = alpha*pow(halomass,beta)
+
+    if fesc_low > fesc_high:
+        fesc[fesc > fesc_low] = fesc_low
+        fesc[fesc < fesc_high] = fesc_high
+    else:
+        fesc[fesc < fesc_low] = fesc_low
+        fesc[fesc > fesc_high] = fesc_high
+
+    ax1.plot(np.log10(halomass),
+             fesc, ls = '--', color = 'r',
+             label = "Mine")
+
+    ax1.set_xlabel("Halo Mass [Msun]")
+    ax1.set_ylabel("fesc")
+
+    ax1.set_ylim([0.0, 1.1])
+
+    return ax1
+
+def plot_MHs(MH_low, MH_high, fesc_low, fesc_high):
+
+    fig1 = plt.figure(figsize = (8,8))
+    ax1 = fig1.add_subplot(111)
+
+    if fesc_high > fesc_low:
+        pos_scaling = 1
+    else:
+        pos_scaling = 0
+    ax1 = plot_Anne_MH(MH_low, MH_high, fesc_low, fesc_high, pos_scaling, ax1)
+    ax1 = plot_my_MH(MH_low, MH_high, fesc_low, fesc_high, ax1)
+
+    leg = ax1.legend(loc='upper right', numpoints=1, labelspacing=0.1)
+    leg.draw_frame(False)  # Don't want a box frame
+    for t in leg.get_texts():  # Reduce the size of the text
+        t.set_fontsize(10)
+
+    outputFile1 = "./fescMH_{0:.2e}_{1:.2e}_{2}_{3}.png".format(MH_low,
+                                                                MH_high,
+                                                                fesc_low,
+                                                                fesc_high)
+    fig1.savefig(outputFile1, bbox_inches='tight')  # Save the figure
+    print('Saved file to {0}'.format(outputFile1))
+    plt.close(fig1)
+
+if __name__ == "__main__":
+
+    MH_low = 1.0e8
+    MH_high = 1.0e12
+    fesc_high = 0.05
+
+    for fesc_low in [0.95]:
+        plot_MHs(MH_low, MH_high, fesc_low, fesc_high)
