Fixed up error calculation for slope.

output/GalaxyPlots.py
@@ -189,8 +189,8 @@ def plot_mstar_fesc(mstar_bins, mstar_bin_width,
     else:
         nrows = int(np.ceil(np.sqrt(len(plot_snaps_for_models)))) 
 
-    fig, ax = plt.subplots(nrows=nrows, ncols=nrows, 
-                           sharex='col', sharey='row', figsize=(16,6))
+    fig1, ax = plt.subplots(nrows=nrows, ncols=nrows, 
+                            sharex='col', sharey='row', figsize=(16,6))
 
     row_ax = -1
     for count, model_number in enumerate(range(len(z_array_full_allmodels))):
@@ -262,9 +262,9 @@ def plot_mstar_fesc(mstar_bins, mstar_bin_width,
     plt.subplots_adjust(wspace = 0.0, hspace = 0.0)
 
     outputFile1 = "{0}/{1}.{2}".format(output_dir, output_tag, output_format)
-    fig.savefig(outputFile1, bbox_inches='tight')  # Save the figure
+    fig1.savefig(outputFile1, bbox_inches='tight')  # Save the figure
     print('Saved file to {0}'.format(outputFile1))
-    plt.close(fig)
+    plt.close(fig1)
 
 
 def plot_SMF(mstar_bins, mstar_bin_width, z_array_full_allmodels,
@@ -320,8 +320,8 @@ def plot_SMF(mstar_bins, mstar_bin_width, z_array_full_allmodels,
     None. The figure is saved as "<output_dir>/<output_tag>.<output_format>".
     """
 
-    fig, ax = plt.subplots(nrows=1, ncols=len(plot_z), 
-                           sharex='col', sharey='row', figsize=(16,6))
+    fig1, ax = plt.subplots(nrows=1, ncols=len(plot_z), 
+                            sharex='col', sharey='row', figsize=(16,6))
 
     for model_number in range(len(z_array_full_allmodels)):
         # Here we find the snapshots closest to the redshifts we want to plot.
@@ -388,9 +388,9 @@ def plot_SMF(mstar_bins, mstar_bin_width, z_array_full_allmodels,
     plt.tight_layout()
 
     outputFile1 = "{0}/{1}.{2}".format(output_dir, output_tag, output_format)
-    fig.savefig(outputFile1, bbox_inches='tight')  # Save the figure
+    fig1.savefig(outputFile1, bbox_inches='tight')  # Save the figure
     print('Saved file to {0}'.format(outputFile1))
-    plt.close(fig)
+    plt.close(fig1)
 
 
 def plot_mstar_fej(mstar_bins, mstar_bin_width, z_array_full_allmodels,
@@ -452,7 +452,7 @@ def plot_mstar_fej(mstar_bins, mstar_bin_width, z_array_full_allmodels,
     None. The figure is saved as "<output_dir>/<output_tag>.<output_format>".
     """
 
-    fig, ax, nrows = plot_2D_line(mstar_bins, mstar_bin_width,
+    fig1, ax, nrows = plot_2D_line(mstar_bins, mstar_bin_width,
                                   z_array_full_allmodels, 
                                   mean_allmodels, std_allmodels, N_allmodels,
                                   model_tags, plot_models_at_snaps,
@@ -495,9 +495,9 @@ def plot_mstar_fej(mstar_bins, mstar_bin_width, z_array_full_allmodels,
     plt.subplots_adjust(wspace = 0.0, hspace = 0.0)
 
     outputFile1 = "{0}/{1}.{2}".format(output_dir, output_tag, output_format)
-    fig.savefig(outputFile1, bbox_inches='tight')  # Save the figure
+    fig1.savefig(outputFile1, bbox_inches='tight')  # Save the figure
     print('Saved file to {0}'.format(outputFile1))
-    plt.close(fig)
+    plt.close(fig1)
 
 
 def plot_mstar_SFR(mstar_bins, mstar_bin_width, z_array_full_allmodels,
@@ -558,10 +558,10 @@ def plot_mstar_SFR(mstar_bins, mstar_bin_width, z_array_full_allmodels,
     None. The figure is saved as "<output_dir>/<output_tag>.<output_format>".
     """
 
-    fig, ax, nrows = plot_2D_line(mstar_bins, mstar_bin_width,
-                                  z_array_full_allmodels, mean_allmodels,
-                                  std_allmodels, N_allmodels, model_tags,
-                                  plot_models_at_snaps, plot_snaps_for_models)
+    fig1, ax, nrows = plot_2D_line(mstar_bins, mstar_bin_width,
+                                   z_array_full_allmodels, mean_allmodels,
+                                   std_allmodels, N_allmodels, model_tags,
+                                   plot_models_at_snaps, plot_snaps_for_models)
 
     delta_fontsize = 10
 
@@ -602,9 +602,9 @@ def plot_mstar_SFR(mstar_bins, mstar_bin_width, z_array_full_allmodels,
     plt.subplots_adjust(wspace = 0.0, hspace = 0.0)
 
     outputFile1 = "{0}/{1}.{2}".format(output_dir, output_tag, output_format)
-    fig.savefig(outputFile1, bbox_inches='tight')  # Save the figure
+    fig1.savefig(outputFile1, bbox_inches='tight')  # Save the figure
     print('Saved file to {0}'.format(outputFile1))
-    plt.close(fig)
+    plt.close(fig1)
 
 
 def plot_2D_line(bins, bin_width, binning_array_allmodels,
@@ -648,7 +648,7 @@ def plot_2D_line(bins, bin_width, binning_array_allmodels,
     Returns
     ---------
 
-    fig : ``matplotlib`` Figure
+    fig1 : ``matplotlib`` Figure
         The created figure.
 
     ax : If number models is 1, a single ``matplotlib`` axes. Otherwise, 2D
@@ -666,8 +666,8 @@ def plot_2D_line(bins, bin_width, binning_array_allmodels,
     else:
         nrows = int(np.ceil(np.sqrt(len(plot_snaps_for_models)))) 
 
-    fig, ax = plt.subplots(nrows=nrows, ncols=nrows, 
-                           sharex='col', sharey='row', figsize=(16,6))
+    fig1, ax = plt.subplots(nrows=nrows, ncols=nrows, 
+                            sharex='col', sharey='row', figsize=(16,6))
 
     row_ax = -1
     for count, model_number in enumerate(range(len(binning_array_allmodels))):
@@ -704,4 +704,4 @@ def plot_2D_line(bins, bin_width, binning_array_allmodels,
                                  ps.global_major_ticklength,
                                  ps.global_minor_ticklength)
 
-    return fig, ax, nrows
+    return fig1, ax, nrows

output/ReionData.py
@@ -831,6 +831,8 @@ def calc_scale_power(k_allmodels, P21_allmodels, PHII_allmodels,
         SKA, specified by the user in ``paper_plots.py``) has at the small and
         large scales. 
 
+        If ``None``, then the errors will not be calculated.
+
     calc_beta : Boolean, optional.
         Flag to denote whether we need to calculate the slope between the large
         and small scale 21cm spectra.
@@ -942,6 +944,7 @@ def calc_scale_power(k_allmodels, P21_allmodels, PHII_allmodels,
 
         P21_beta_error = []
 
+
         for model_number in range(num_models):
 
             P21_beta.append([])
@@ -975,11 +978,22 @@ def calc_scale_power(k_allmodels, P21_allmodels, PHII_allmodels,
                 PHII_beta_snap = (PHII_large_snap - PHII_small_snap) / \
                                  (k_large_snap - k_small_snap)
 
-                # Assume that there's no uncertainty in the k-values. Then
-                # delta(Slope)/slope = sqrt((delta(P_Large)/P_large)^2 + (delta(P_small)/P_small)^2)
-                P21_error_snap = P21_beta_snap * np.sqrt((large_scale_err/P21_large_snap)**2 + \
-                                                         (small_scale_err/P21_small_snap)**2)
 
+                # If there's no error defined, skip this calculation. 
+                if not small_scale_err or not large_scale_err:
+                    P21_beta_error = None
+                    continue
+
+                # Assume that there's no uncertainty in the k-values. Then
+                # delta(Slope) is sqrt(large scale error^2 + small scale
+                # error^2) divided by the difference in scales.
+                P21_error_snap = np.sqrt(large_scale_err**2 + \
+                                         small_scale_err**2) / \
+                                 (k_large_snap - k_small_snap)
+                # Error can only be positive...
+                P21_error_snap = np.abs(P21_error_snap)
+
+                debug = True
                 if debug:
                     print("")
                     print("Snap {0}".format(snap_idx))

output/ReionPlots.py
@@ -284,10 +284,10 @@ def plot_ps_fixed_XHI(k, P21, PHII, fixed_XHI_values, model_tags, output_dir,
     None. The figure is saved as "<output_dir>/<output_tag>.<output_format>".
     """
 
-    fig, ax = plt.subplots(nrows = 1, ncols = len(fixed_XHI_values),
-                           sharey='row', figsize=(16, 6))
+    fig1, ax = plt.subplots(nrows=1, ncols=len(fixed_XHI_values),
+                            sharey='row', figsize=(16, 6))
 
-    fig2, ax2 = plt.subplots(nrows = 1, ncols = len(fixed_XHI_values),
+    fig2, ax2 = plt.subplots(nrows=1, ncols=len(fixed_XHI_values),
                              sharey='row', figsize=(16, 6))
 
     for model_number in range(len(k)):
@@ -375,8 +375,8 @@ def plot_ps_fixed_XHI(k, P21, PHII, fixed_XHI_values, model_tags, output_dir,
         t.set_fontsize(ps.global_legendsize-2)
     leg2.draw_frame(False)  # Don't want a box frame
 
-    fig.tight_layout()
-    fig.subplots_adjust(wspace = 0.0, hspace = 0.0)
+    fig1.tight_layout()
+    fig1.subplots_adjust(wspace = 0.0, hspace = 0.0)
 
     fig2.tight_layout()
     fig2.subplots_adjust(wspace = 0.0, hspace = 0.0)
@@ -384,7 +384,7 @@ def plot_ps_fixed_XHI(k, P21, PHII, fixed_XHI_values, model_tags, output_dir,
     outputFile = "{0}/{1}.{2}".format(output_dir,
                                      output_tag,
                                      output_format)
-    fig.savefig(outputFile)
+    fig1.savefig(outputFile)
     print('Saved file to {0}'.format(outputFile))
 
     outputFile = "{0}/{1}_HII.{2}".format(output_dir,
@@ -543,7 +543,7 @@ def plot_tau_contours(tau_highz, reion_completed, alpha_beta_limits,
                         fontsize = ps.global_fontsize)
 
     outputFile = "{0}/{1}.{2}".format(output_dir, output_tag, output_format)
-    plt.savefig(outputFile, bbox_inches='tight')  # Save the figure
+    fig1.savefig(outputFile, bbox_inches='tight')  # Save the figure
     print('Saved file to {0}'.format(outputFile))
     plt.close()
 
@@ -639,7 +639,7 @@ def plot_duration_contours(duration_t, reion_completed, alpha_beta_limits,
                         fontsize = ps.global_fontsize)
 
     outputFile = "{0}/{1}.{2}".format(output_dir, output_tag, output_format)
-    plt.savefig(outputFile, bbox_inches='tight')  # Save the figure
+    fig1.savefig(outputFile, bbox_inches='tight')  # Save the figure
     print('Saved file to {0}'.format(outputFile))
     plt.close()
 
@@ -736,7 +736,7 @@ def plot_tau(z_array_reion_allmodels, lookback_array_reion_allmodels,
     ax1.axhline(y = 0.058, xmin = 0, xmax = 20, color = 'k', alpha = 0.3)
     ax1.text(850, 0.0570, r"$\mathrm{Planck \: 2016}$")
 
-    plot_planck_2018 = 0
+    plot_planck_2018 = 1
     if plot_planck_2018:
         ax1.fill_between(np.arange(200, 1200, 0.01), 0.054 - 0.007, 0.054 + 0.007,
                          color = 'c', alpha = 0.3)
@@ -753,7 +753,7 @@ def plot_tau(z_array_reion_allmodels, lookback_array_reion_allmodels,
         return ax1
     else:
         outputFile = "{0}/{1}.{2}".format(output_dir, output_tag, output_format)
-        plt.savefig(outputFile, bbox_inches='tight')  # Save the figure
+        fig1.savefig(outputFile, bbox_inches='tight')  # Save the figure
         print('Saved file to {0}'.format(outputFile))
         plt.close()
 
@@ -815,7 +815,7 @@ def plot_combined_history_tau(z_array_reion_allmodels,
     None. The figure is saved as "<output_dir>/<output_tag>.<output_format>".
     """
 
-    fig, ax = plt.subplots(nrows = 1, ncols = 2, 
+    fig1, ax = plt.subplots(nrows = 1, ncols = 2, 
                            sharex=False, sharey=False, figsize=(16, 8))
 
     # Pass each axis to the correct function to create the plots.
@@ -838,7 +838,7 @@ def plot_combined_history_tau(z_array_reion_allmodels,
     plt.tight_layout()    
 
     outputFile = "{0}/{1}.{2}".format(output_dir, output_tag, output_format)
-    plt.savefig(outputFile, bbox_inches='tight')  # Save the figure
+    fig1.savefig(outputFile, bbox_inches='tight')  # Save the figure
     print('Saved file to {0}'.format(outputFile))
     plt.close()
 
@@ -1034,7 +1034,7 @@ def plot_ps_scales(P21_small_scale, P21_large_scale,
     outputFile = "{0}/{1}_{2}.{3}".format(output_dir, tag, name_tag,
                                           output_format)
 
-    plt.savefig(outputFile, bbox_inches='tight')  # Save the figure
+    fig1.savefig(outputFile, bbox_inches='tight')  # Save the figure
     print('Saved file to {0}'.format(outputFile))
     plt.close()
 
@@ -1117,9 +1117,9 @@ def plot_slices_XHI(z_array_reion_allmodels, cosmology_allmodels,
     num_models = len(mass_frac_allmodels)
     num_fractions = len(fixed_XHI_values)
 
-    fig, ax = plt.subplots(nrows=num_fractions, ncols=num_models,
+    fig1, ax = plt.subplots(nrows=num_fractions, ncols=num_models,
                            sharey=False, sharex=False, figsize=(12, 12))
-    fig.subplots_adjust(wspace = 0.02, hspace = 0.02)
+    fig1.subplots_adjust(wspace = 0.02, hspace = 0.02)
 
     # Each model can have a different gridsize.  We want to cut at the same
     # spatial location for each model so we will normalize to model 0.
@@ -1197,9 +1197,9 @@ def plot_slices_XHI(z_array_reion_allmodels, cosmology_allmodels,
     # Model Loop.
 
     # All the models have been plotted. Now lets fix up the colorbar.
-    cax = fig.add_axes([0.91, 0.11, 0.03, 0.77])
+    cax = fig1.add_axes([0.91, 0.11, 0.03, 0.77])
     ticks = np.arange(-8.0, 1.0, 1.0)
-    cbar = fig.colorbar(im, cax=cax, ticks=ticks) 
+    cbar = fig1.colorbar(im, cax=cax, ticks=ticks) 
     cbar.ax.set_yticklabels([r"$\mathbf{%d}$" % x for x in ticks], 
                             fontsize = ps.global_legendsize+10)
     cbar.ax.set_ylabel(r'$\mathbf{log_{10}\left(\chi_{HI}\right)}$',
@@ -1208,7 +1208,7 @@ def plot_slices_XHI(z_array_reion_allmodels, cosmology_allmodels,
 
     # Done! Save time.
     outputFile = "{0}/{1}.{2}".format(output_dir, output_tag, output_format)
-    plt.savefig(outputFile, bbox_inches='tight')  # Save the figure
+    fig1.savefig(outputFile, bbox_inches='tight')  # Save the figure
     print('Saved file to {0}'.format(outputFile))
     plt.close()
 
@@ -1523,14 +1523,14 @@ def plot_dens_reion_contours(mass_frac_allmodels, XHII_fbase_allmodels,
 
         #c.configure(plot_hists=False)
 
-        fig = c.plotter.plot(figsize="column")
+        fig1 = c.plotter.plot(figsize="column")
 
         final_tag = "{0}_XHI{1}".format(output_tag, frac_val)
 
         outputFile = "{0}/{1}.{2}".format(output_dir,
                                           final_tag, 
                                           output_format)
-        fig.savefig(outputFile)
+        fig1.savefig(outputFile)
         print('Saved file to {0}'.format(outputFile))
 
 
@@ -1620,7 +1620,7 @@ def plot_dens_zreion_contours(density_fbase_allmodels,
 
     c.configure(sigmas=[0, 1, 2, 3])
 
-    fig = c.plotter.plot(figsize="column")
+    fig1 = c.plotter.plot(figsize="column")
 
     #ax = fig.get_axes()
 
@@ -1633,16 +1633,16 @@ def plot_dens_zreion_contours(density_fbase_allmodels,
     ax1.set_ylabel(
                    size = ps.global_labelsize)
     '''
-    #fig.tight_layout()
+    #fig1.tight_layout()
     outputFile = "{0}/{1}.{2}".format(output_dir,
                                       output_tag, 
                                       output_format)
 
-    fig.savefig(outputFile)
+    fig1.savefig(outputFile)
     print('Saved file to {0}'.format(outputFile))
 
 
-def plot_ps_beta(P21_beta, P21_beta_error, mass_frac_allmodels,
+def plot_ps_beta(P21_beta, P21_beta_error,
                  z_array_reion_allmodels, lookback_array_reion_allmodels,
                  cosmology_allmodels, t_bigbang_allmodels, small_scale_def,
                  large_scale_def, model_tags, output_dir, output_tag,
@@ -1662,6 +1662,8 @@ def plot_ps_beta(P21_beta, P21_beta_error, mass_frac_allmodels,
                                of models. Inner is number of snapshots.
         The slope (and error) between large scale and small scale (given by 
         ``small_scale_def`` and ``large_scale_def``) 21cm power spectra.
+
+        If ``P21_beta_error`` is ``None``, then no errors will be plotted.
 
     z_array_reion_allmodels : 2D nested list of floats. Outer length is number
                               of models, inner is number of snapshots.
@@ -1720,13 +1722,19 @@ def plot_ps_beta(P21_beta, P21_beta_error, mass_frac_allmodels,
                  label=model_tags[model_number],
                  zorder=1)
 
-        # Want to add shaded regions that correspond to error.
-        this_err = np.array(P21_beta_error[model_number])
-        beta_err_lower = this_beta - this_err 
-        beta_err_upper = this_beta + this_err
+        # If we've calculated error, add it to the plot.
+        if P21_beta_error:
+            this_err = np.array(P21_beta_error[model_number])
+            beta_err_lower = this_beta - this_err 
+            beta_err_upper = this_beta + this_err
 
-        ax1.fill_between(lookback, beta_err_lower, beta_err_upper,
-                         facecolor=ps.colors[model_number], alpha=0.25)
+            print(beta_err_lower)
+            print(beta_err_upper)
+            print(beta_err_upper - beta_err_lower)
+            print(lookback)
+
+            ax1.fill_between(lookback, beta_err_lower, beta_err_upper,
+                             facecolor=ps.colors[model_number], alpha=0.25)
 
     ax2 = ps.add_time_z_axis(ax1, cosmology_allmodels[0],
                              t_bigbang_allmodels[0]/1.0e3, ps.time_xlim)
@@ -1734,8 +1742,9 @@ def plot_ps_beta(P21_beta, P21_beta_error, mass_frac_allmodels,
     ax1.set_xlabel(r"$\mathbf{Time \: since \: Big \: Bang \: [Myr]}$", 
                    fontsize = ps.global_labelsize)
     ax1.set_ylabel(r"$\mathbf{Slope \: [mK^2 h^{-1}Mpc]}$",
-                   fontsize = ps.global_labelsize) 
-    ax1.axhline(y=0, xmin=0, xmax=np.amax(lookback), lw=0.5, ls='--', color='k')
+                   fontsize = ps.global_labelsize)
+
+    ax1.axhline(y=0, xmin=0, xmax=max(ps.time_xlim), lw=0.5, ls='--', color='k')
 
     ax1 = ps.adjust_axis(ax1, ps.global_axiswidth, ps.global_tickwidth,
                          ps.global_major_ticklength, ps.global_minor_ticklength) 
@@ -1751,6 +1760,6 @@ def plot_ps_beta(P21_beta, P21_beta_error, mass_frac_allmodels,
                                                    large_scale_def)
 
     outputFile = "{0}/{1}.{2}".format(output_dir, tag, output_format)
-    plt.savefig(outputFile, bbox_inches='tight')  # Save the figure
+    fig1.savefig(outputFile, bbox_inches='tight')  # Save the figure
     print('Saved file to {0}'.format(outputFile))
     plt.close()